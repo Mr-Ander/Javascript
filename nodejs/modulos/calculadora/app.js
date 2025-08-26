45def972334504bbc3b55736421714cf4e28d130
Aluno IFC <aluno@lab32-pc01.sombrio.ifc.edu.br>
2025-08-26 09:29:44 -0300

    vcalculadora

---
+++ nodejs/modulos/calculadora/app.js
@@ -0,0 +1,8 @@
+const { soma, subtrai, multiplica, divide } = require('./funcoes_matematicas');
+
+// Exemplos de uso:
+console.log('Soma:', soma(5, 3));
+console.log('Subtração:', subtrai(10, 4));
+console.log('Multiplicação:', multiplica(2, 7));
+console.log('Divisão:', divide(20, 5));// Evitando divisão por zero 
+
